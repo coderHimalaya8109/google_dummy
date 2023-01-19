55eb25ec9bbbfe55888a13047b87896a1087b126
Himalaya Creation <>
2023-01-19 16:20:08 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function demof(){
+  let s = document.getElementById("search_bar").value;
+  alert(s)
+}
+function hiddenf(){
+let h = document.getElementById("hidden_box");
+  if(h.style.display === "none"){
+    h.style.display = "block";
+  }else{
+    h.style.display = "none"
+  }
+}
+function hiddenf1(){
+  let h1 = document.getElementById("hidden_box");
+  h1.style.display = "none";
+}
